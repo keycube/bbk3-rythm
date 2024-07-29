54f7a9bab56ee27963f763be34447b11e142a241
Antoine Lafitte <Antoine.Lafitte10@outlook.fr>
2024-07-29 11:41:41 -0400

    changed some comments again

--- BBK3-code/Arduino/sketch_gamepadkeyboardhid/sketch_gamepadkeyboardhid.ino
+++ BBK3-code/Arduino/sketch_gamepadkeyboardhid/sketch_gamepadkeyboardhid.ino
@@ -7,14 +7,14 @@
 #define MAGENTA 0xFF00FF
 #define GREEN 0x33FF33
 
-
+// report IDs' enum
 enum
 {
   RID_GAMEPAD = 1,
   RID_KEYBOARD,
 };
+
 // HID report descriptor using TinyUSB's template
-// Single Report (no ID) descriptor
 uint8_t const desc_hid_report[] =
 {
   TUD_HID_REPORT_DESC_KEYBOARD( HID_REPORT_ID(RID_KEYBOARD) ),
@@ -34,13 +34,11 @@ int switch_time = 5000;
 Adafruit_USBD_HID usb_hid(desc_hid_report, sizeof(desc_hid_report), HID_ITF_PROTOCOL_NONE, 2, false);
 hid_gamepad_report_t    gp;
 
-// USB HID object. For ESP32 these values cannot be changed after this declaration
-// desc report, desc len, protocol, interval, use out endpoint
 LSM6DS3 myIMU(I2C_MODE, 0x6A); 
 
 
 //------------- Input Pins -------------//
-// Array of pins and its keycode.
+// Array of pins
 // Notes: these pins can be replaced by PIN_BUTTONn if defined in setup()
 #ifdef ARDUINO_ARCH_RP2040
   uint8_t pins[] = { D1, D2, D3, D4, D5 };
@@ -51,10 +49,8 @@ LSM6DS3 myIMU(I2C_MODE, 0x6A);
 // number of pins
 uint8_t pincount = sizeof(pins);
 
-// For keycode definition check out https://github.com/hathach/tinyusb/blob/master/src/class/hid/hid.h
-//uint8_t hidcode[] = { HID_KEY_W, HID_KEY_A, HID_KEY_S, HID_KEY_D, HID_KEY_X };
-
-int colorsHex[] = { MAGENTA, MAGENTA, MAGENTA, 0x33FF33, BLACK};
+// colors for each neopixel pin
+int colorsHex[] = { MAGENTA, MAGENTA, MAGENTA, GREEN, BLACK};
 
 #if defined(ARDUINO_SAMD_CIRCUITPLAYGROUND_EXPRESS) || defined(ARDUINO_NRF52840_CIRCUITPLAY) || defined(ARDUINO_FUNHOUSE_ESP32S2)
   bool activeState = true;
